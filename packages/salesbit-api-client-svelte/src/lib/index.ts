2d8dd2f1198f47fdec3a7311212a35e6dc131d6d
Ilya Frolov <023150@gmail.com>
2024-01-10 16:24:19 +0100

    Fix

--- packages/salesbit-api-client-svelte/src/lib/index.ts
+++ packages/salesbit-api-client-svelte/src/lib/index.ts
@@ -1,2 +1,3 @@
 export { default as ProductProperties } from './product/Properties.svelte';
 export { default as ProductPrice } from './product/Price.svelte';
+export { default as User } from './product/Price.svelte';
