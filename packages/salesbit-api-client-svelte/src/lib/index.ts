94c7552759512436f2ebb9654bbac4de5e1249c2
Ilya Frolov <023150@gmail.com>
2024-01-11 11:05:44 +0100

    Demo

--- packages/salesbit-api-client-svelte/src/lib/index.ts
+++ packages/salesbit-api-client-svelte/src/lib/index.ts
@@ -2,3 +2,4 @@ export { default as Properties } from './product/Properties.svelte';
 export { default as Price } from './product/Price.svelte';
 export { default as Checkout } from './Checkout.svelte';
 export { default as User } from './User.svelte';
+export { default as User2 } from './User2.svelte';
